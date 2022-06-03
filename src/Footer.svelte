099a0401a153b78d2c75f5e502ec97085285d878
asdfghjkkl11 <asdfghjkkl11@naver.com>
2022-06-03 16:24:13 +0900

    footer링크 추가

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -1,5 +1,7 @@
 <div class="footer">
-    Copyright © 2022 asdfghjkkl11
+    <span>Copyright © 2022 asdfghjkkl11</span>
+    <a href="https://discord.gg/rg4kxmVuCZ" target="_blank"><img src="/image/discord.svg"></a>
+    <a href="http://donate.asdfghjkkl11.com" target="_blank">후원하기</a>
 </div>
 <style>
     .footer{
@@ -7,6 +9,11 @@
         display: flex;
         align-items: center;
         justify-content: center;
+        gap: 8px;
+        color: #b8b8b8;
+    }
+    a{
+        text-decoration:none;
         color: #b8b8b8;
     }
 </style>
